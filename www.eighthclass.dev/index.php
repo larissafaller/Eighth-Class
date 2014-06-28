e1e31e30e5c844efc36deb99586b2bbbcef382ff
Larissa Faller <larissafaller@yahoo.com>
2014-06-28 17:41:19 +0800

    Form working (sorta)
    Still getting Notice: Undefined index for “button” , “name”  , “phone”.

--- www.eighthclass.dev/index.php
+++ www.eighthclass.dev/index.php
@@ -9,45 +9,80 @@
 	<?php 
 	
 
-	// If "button" has been pressed AND if $name, $phone values are set { 
-	// echo Thank you for your information. } 
-	// else { show error message with form. }
+// 	if (submit button has been pressed) {
+// 			and if ($name, $phone values are set) { echo thank you. }
+// 			and if (value/s is/are empty) { show form with error message. }
+// }	else { show form. 
+// }
+
+
 	$phone = $_GET["phone_number"];
 
 	$name = $_GET["full_name"];
 
-	if ( $_GET["button"] AND !empty($name) AND !empty($phone) ) {
-		echo "Thank you for your information <br>";
-	} 
+	if ( $_GET["button"] ){
 
-	if ( $_GET["button"] AND empty($name) AND empty($phone) ) {
+		if ( !empty($name) AND !empty($phone) ) {
 
-		echo "There was a problem with your information. Please try again. <br>"; ?>
+			echo "Thank you for your information <br>"; }
 
-		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
-			<div class="row">
-				<div class="large-12 columns">
-					<label>Full Name
-						<input type="text" placeholder="Your Name" name="full_name"/>
-					</label>
+			if ( empty($name) AND empty($phone) ) {
+
+				echo "There was a problem with your information. Please try again. <br>"; ?>
+
+				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+					<div class="row">
+						<div class="large-12 columns">
+							<label>Full Name
+								<input type="text" placeholder="Your Name" name="full_name"/>
+							</label>
+						</div>
+					</div>
+					<div class="row">
+						<div class="large-4 columns">
+							<label>Phone Number						
+								<input type="text" placeholder="123-5555" name="phone_number"/>
+							</label>
+						</div>
+					</div>
+					<div class="row">
+						<div class="large-4 columns">
+							<input type="submit" value="Submit" name="button"/>
+						</div>
+					</div>
+				</form>
+
+				<?php } 
+
+			} else { ?>
+
+			<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+				<div class="row">
+					<div class="large-12 columns">
+						<label>Full Name
+							<input type="text" placeholder="Your Name" name="full_name"/>
+						</label>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="large-4 columns">
-					<label>Phone Number						
-						<input type="text" placeholder="123-5555" name="phone_number"/>
-					</label>
+				<div class="row">
+					<div class="large-4 columns">
+						<label>Phone Number						
+							<input type="text" placeholder="123-5555" name="phone_number"/>
+						</label>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="large-4 columns">
-					<input type="submit" value="Submit" name="button"/>
+				<div class="row">
+					<div class="large-4 columns">
+						<input type="submit" value="Submit" name="button"/>
+					</div>
 				</div>
-			</div>
-		</form>
+			</form>
 
-		<?php } ?>
+			<?php } 
+
+			
+			?>
 
 
-	</body>
-	</html>
+		</body>
+		</html>

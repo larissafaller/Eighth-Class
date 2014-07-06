3fd9870d7196cf594613963fdfa27fb6d0cbe747
Larissa Faller <larissafaller@yahoo.com>
2014-07-06 14:26:59 +0800

    New Form
    New form seems to be working.

--- www.eighthclass.dev/index.php
+++ www.eighthclass.dev/index.php
@@ -23,83 +23,114 @@
 			if ( isset($_GET["phone_number"]) ){ $clean_number = str_replace($bad_items, "", $_GET["phone_number"]); }
 
 
-
 // 	1. if (submit button has been pressed) {
 			if ( isset($_GET["submit_button"]) ) {
 
-//  	2a. if 1. and if (name AND number are both NOT empty) {
-				if ( !empty($clean_name) AND !empty($clean_number) ) {
 
-//  		3a. if 1. and 2. and if there is no space between the words and the number is numeric {
-					if ( stripos($_GET["full_name"], " ") == true AND is_numeric($clean_number) ) { 
+// if (name is  empty OR name doesn't contain a space) {
+				if ( empty($clean_name) ){
+					$errors[] = '<h4 class="text-center">Please fill in your name.</h4><br>'; 
+				}
 
-//  			4a. if 1. and 2. and 3. then echo success message, show puppy; }}
-						echo "<h4 class=\"text-center\">Success!<br>Thank you for your information.</h4><br><br>";
-						?>
-						<div class="row">
-							<div class="large-6 columns large-centered">
-								<img src="img/smiling-pug.jpg" alt="Smiling Pug" class="th"> 
+				if ( !empty($clean_name) AND ( stripos($_GET["full_name"], " ") === false ) ){
+					$errors[] = '<h4 class="text-center">Please fill in your full name. (e.g. John Smith)</h4><br>'; 
+				}
+
+
+// if number is not empty OR the number is numeric {
+				if ( empty($clean_number) ) {
+					$errors[] = '<h4 class="text-center">Please fill in your phone number.</h4><br>'; 
+				}
+
+				if ( !empty($clean_number) AND !is_numeric($clean_number) ) {
+					$errors[] = '<h4 class="text-center">Please fill in your correct phone number. (e.g. 640-3978)</h4><br>'; 
+				}
+			}
+
+
+
+
+
+// 2. If there are any error messages in the array, print them all.
+
+			if ( isset ($errors[0]) ) { echo $errors[0]; }
+
+			if ( isset ($errors[1]) ) { echo $errors[1]; }
+
+			if ( isset ($errors[2]) ) { echo $errors[2]; }
+
+			if ( isset ($errors[3]) ) { echo $errors[3]; }
+
+
+
+
+
+
+// 3. If submit button has been pressed and there are no errors:
+
+
+			if ( isset($_GET["submit_button"]) AND ( !isset($errors) )) {
+
+
+// then echo success message, show puppy; }
+				echo "<h4 class=\"text-center\">Success!<br>Thank you for your information.</h4><br><br>";
+				?>
+				<div class="row">
+					<div class="large-6 columns large-centered">
+						<img src="img/smiling-pug.jpg" alt="Smiling Pug" class="th"> 
+					</div>
+				</div>
+				<?php
+			} else {
+
+
+// Display the form.
+				if ( !isset($_GET["submit_button"]) OR ( isset($errors)) ) {
+					?>
+					<div class="row">
+						<div class="large-6 columns large-centered">
+							<div class=" panel">
+								<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+									<div class="row">
+										<div class="large-12 columns">
+											<label>Full Name</label>
+											<input type="text" placeholder="Your Name" name="full_name" 
+
+											<?php if ( isset($_GET["full_name"]) ) {
+												echo "value='" . $_GET["full_name"] . "' ";
+											} ?>
+
+											/>
+										</div>
+									</div>
+									<div class="row">
+										<div class="large-12 columns">
+											<label>Phone Number</label>					
+											<input type="text" placeholder="123-4567" name="phone_number"
+
+											<?php if ( isset($_GET["phone_number"]) ) {
+												echo "value='" . $_GET["phone_number"] . "' ";
+											} ?>
+
+											/>
+
+										</div>
+									</div>
+									<div class="row">
+										<div class="large-4 columns large-centered">
+											<input type="submit" value="Submit" name="submit_button" class=" medium button radius"/>
+										</div>
+									</div>
+								</form>
 							</div>
 						</div>
-						<?php
-					}
+					</div>
+					<?php 
 				}
-
-//  	2b. if 1. and if (name OR number is empty) {
-
-				if ( empty($clean_name) OR empty($clean_number) )  {
-
-// 			3b. if 1. and 2b. if full name does have a space, but phone number is not numeric  {
-					if ( stripos($_GET["full_name"], " ") === false OR !is_numeric($clean_number)) {
-
-// 			4b. if 1. and 2b. and 3b. , then echo error message and show form}
-						echo "<h4 class=\"text-center\">Uh-Oh! Something's missing.<br>Please try again.</h4><br>"; 
-						include 'form.php'; 
-						die();
-					} 
-				}
-
-// 		2c. if 1. and if name and number are NOT empty {
-				if ( !empty($clean_name) AND !empty($clean_number) ) {
-
-// 			3c. if 1. and 2c. and if full name doesn't have a space, but number is numeric {
-					if ( stripos($_GET["full_name"], " ") === false AND is_numeric($clean_number)) {
-
-// 				4c. if 1. and 2c. and 3c. , then echo 'full name' error message and show form. 
-						echo "<h4 class=\"text-center\">Oops! It seems there was a problem.<br>Please input your full name.</h4><br>";
-						include 'form.php';  
-					}
-
-// 			3cc. if 1. and 2c. if full name does have a space, but phone number is not numeric  {
-					if ( stripos($_GET["full_name"], " ") == true AND !is_numeric($clean_number) ) {
-
-// 				4d. if 1. and 2c. and 3cc. , then echo 'phone number' error message and show form. 
-						echo "<h4 class=\"text-center\">Oops! It seems there was a problem.<br>Please input a correct phone number.</h4><br>";
-						include 'form.php'; 
-					}
-				}
-
-// 		2d. if 1. and if name OR number are empty {
-
-				if ( !empty($clean_name) OR !empty($clean_number) )  {
-
-// 			3d. if 1. and 2d. if full name doesn't have a space and phone number is not numeric {
-					if ( stripos($_GET["full_name"], " ") === false AND !is_numeric($clean_number) ) {
+			}
 
 
-// 				4d. if 1. and 2d. and 3d. , then echo error 'full name and phone number' message and show form. 
-						echo "<h4 class=\"text-center\">Oops! It seems there was a problem.<br>Please input your full name and a correct phone number.</h4><br>";
-						include 'form.php'; 
-
-					}
-				}
-
-
-// If not 1, show start up message and show form;
-			} else { 
-				echo "<h4 class=\"text-center\">Hello!<br>Please give your full name and phone number.<br></h4><br>";
-				include 'form.php'; 
-			} ?>
+			?>
 
 
 		</div>

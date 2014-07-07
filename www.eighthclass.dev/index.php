34c81e3c04492de94883f075eba03c8364da4b2a
Larissa Faller <larissafaller@yahoo.com>
2014-07-07 20:57:43 +0800

    Form with second screen (incomplete)
    Problems:
    1. Doesn’t validate second form.
    2. Bit of a mess.
    3. Driving me mental.

--- www.eighthclass.dev/index.php
+++ www.eighthclass.dev/index.php
@@ -16,60 +16,64 @@
 			
 			<?php 
 
+			
+
 			if ( isset($_GET["full_name"]) ){ $clean_name = preg_replace("/[^A-Za-z?! ]/","", $_GET["full_name"]); }
 
 			$bad_items = array("-", " ", "(", ")", ".");
 
 			if ( isset($_GET["phone_number"]) ){ $clean_number = str_replace($bad_items, "", $_GET["phone_number"]); }
 
+			if ( isset($_GET["breakfast"]) ){ $clean_breakfast = preg_replace("/[^A-Za-z?! ]/","", $_GET["breakfast"]); }
 
-// 	1. if (submit button has been pressed) {
-			if ( isset($_GET["submit_button"]) ) {
+			if ( isset($_GET["dinner"]) ){ $clean_dinner = preg_replace("/[^A-Za-z?! ]/","", $_GET["dinner"]); }
 
 
-// if (name is  empty OR name doesn't contain a space) {
+
+// 	if (submit button1 has been pressed) {
+			if ( isset($_GET["submit_button1"]) ) {
+
+
+
+// if (name is empty OR name doesn't contain a space) { set errors1[] }
 				if ( empty($clean_name) ){
-					$errors[] = '<h4 class="text-center">Please fill in your name.</h4><br>'; 
+					$errors1[] = '<h4 class="text-center">Please fill in your name.</h4><br>'; 
 				}
 
 				if ( !empty($clean_name) AND ( stripos($_GET["full_name"], " ") === false ) ){
-					$errors[] = '<h4 class="text-center">Please fill in your full name. (e.g. John Smith)</h4><br>'; 
+					$errors1[] = '<h4 class="text-center">Please fill in your full name. (e.g. John Smith)</h4><br>'; 
 				}
 
 
-// if number is not empty OR the number is numeric {
+
+// if (number is empty OR the number is not numeric ) { set error1[] }
 				if ( empty($clean_number) ) {
-					$errors[] = '<h4 class="text-center">Please fill in your phone number.</h4><br>'; 
+					$errors1[] = '<h4 class="text-center">Please fill in your phone number.</h4><br>'; 
 				}
 
 				if ( !empty($clean_number) AND !is_numeric($clean_number) ) {
-					$errors[] = '<h4 class="text-center">Please fill in your correct phone number. (e.g. 640-3978)</h4><br>'; 
+					$errors1[] = '<h4 class="text-center">Please fill in your correct phone number. (e.g. 640-3978)</h4><br>'; 
 				}
 			}
 
 
 
+// if (there are any error1 messages in the array ) { print them all }
 
+			if ( isset ($errors1[0]) ) { echo $errors1[0]; }
 
-// 2. If there are any error messages in the array, print them all.
+			if ( isset ($errors1[1]) ) { echo $errors1[1]; }
 
-			if ( isset ($errors[0]) ) { echo $errors[0]; }
+			if ( isset ($errors1[2]) ) { echo $errors1[2]; }
 
-			if ( isset ($errors[1]) ) { echo $errors[1]; }
-
-			if ( isset ($errors[2]) ) { echo $errors[2]; }
-
-			if ( isset ($errors[3]) ) { echo $errors[3]; }
-
+			if ( isset ($errors1[3]) ) { echo $errors1[3]; }
 
 
 
+// if ( submit button2 has been pressed and there are no errors2 ) {
 
+			if ( isset($_GET["submit_button2"]) AND  !isset($errors2) ) {
 
-// 3. If submit button has been pressed and there are no errors:
-
-
-			if ( isset($_GET["submit_button"]) AND ( !isset($errors) )) {
 
 
 // then echo success message, show puppy; }
@@ -80,45 +84,119 @@
 						<img src="img/smiling-pug.jpg" alt="Smiling Pug" class="th"> 
 					</div>
 				</div>
-				<?php
-			} else {
+				<?php 
+				die();
+			}
 
 
-// Display the form.
-				if ( !isset($_GET["submit_button"]) OR ( isset($errors)) ) {
+// if (submit1 is not set OR there are errors1) { display the form }
+			if ( !isset($_GET["submit_button1"]) OR ( isset($errors1)) ) {
+				?>
+				<div class="row">
+					<div class="large-6 columns large-centered">
+						<div class=" panel">
+							<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+								<div class="row">
+									<div class="large-12 columns">
+										<label>Full Name</label>
+										<input type="text" placeholder="Your Name" name="full_name" 
+
+										<?php if ( isset($_GET["full_name"]) ) {
+											echo "value='" . $_GET["full_name"] . "' ";
+										} ?>
+
+										/>
+									</div>
+								</div>
+								<div class="row">
+									<div class="large-12 columns">
+										<label>Phone Number</label>					
+										<input type="text" placeholder="123-4567" name="phone_number"
+
+										<?php if ( isset($_GET["phone_number"]) ) {
+											echo "value='" . $_GET["phone_number"] . "' ";
+										} ?> />
+									</div>
+								</div>
+								<div class="row">
+									<div class="large-4 columns large-centered">
+										<input type="submit" value="Submit" name="submit_button1" class=" medium button radius"/>
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+				<?php 
+			} 
+
+
+
+// if ( submit button2 has been pressed ) {
+			if ( isset($_GET["submit_button2"]) ) {
+
+
+
+// if breakfast is  empty { set errors2[] }
+				if ( empty($clean_breakfast) ){  
+					$errors2[] = '<h4 class="text-center">Please fill in your favourite breakfast.</h4><br>'; 
+				}
+
+
+
+// if dinner is  empty { set errors2[] }
+				if ( empty($clean_dinner) ){
+					$errors2[] = '<h4 class="text-center">Please fill in your favourite dinner.</h4><br>'; 
+				}
+
+
+
+// if there are any errors2 messages in the array, print them all.
+
+				if ( isset ($errors2[0]) ) { echo $errors2[0]; }
+
+				if ( isset ($errors2[1]) ) { echo $errors2[1]; }
+			}
+
+
+
+// if ( submit1 is set AND there are no erros1) {
+// if ( submit2 is not set OR there are errors2 ) { display form2 }}
+			if ( isset($_GET["submit_button1"]) AND !isset($errors1) ) {
+				if ( !isset($_GET["submit_button2"]) OR isset($errors2) ) {
+
 					?>
 					<div class="row">
 						<div class="large-6 columns large-centered">
 							<div class=" panel">
 								<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+									<input type="hidden" name="full_name" value="Lara Faller">
+									<input type="hidden" name="phone_number" value="123-5555">
 									<div class="row">
 										<div class="large-12 columns">
-											<label>Full Name</label>
-											<input type="text" placeholder="Your Name" name="full_name" 
+											<label>Favourite Breakfast</label>
+											<input type="text" placeholder="Your favourite breakfast" name="breakfast" 
 
-											<?php if ( isset($_GET["full_name"]) ) {
-												echo "value='" . $_GET["full_name"] . "' ";
-											} ?>
-
-											/>
+											<?php if ( isset($_GET["breakfast"]) ) {
+												echo "value='" . $_GET["breakfast"] . "' ";
+											} ?>/>
 										</div>
 									</div>
 									<div class="row">
 										<div class="large-12 columns">
-											<label>Phone Number</label>					
-											<input type="text" placeholder="123-4567" name="phone_number"
+											<label>Favourite Dinner</label>					
+											<input type="text" placeholder="Your favourite dinner" name="dinner"
 
-											<?php if ( isset($_GET["phone_number"]) ) {
-												echo "value='" . $_GET["phone_number"] . "' ";
-											} ?>
-
-											/>
-
+											<?php if ( isset($_GET["dinner"]) ) {
+												echo "value='" . $_GET["dinner"] . "' ";
+											} ?> />
 										</div>
 									</div>
 									<div class="row">
 										<div class="large-4 columns large-centered">
-											<input type="submit" value="Submit" name="submit_button" class=" medium button radius"/>
+
+											<input type="submit" value="Submit" name="submit_button2" class=" medium button radius"/>
+
 										</div>
 									</div>
 								</form>
@@ -128,6 +206,7 @@
 					<?php 
 				}
 			}
+
 
 
 			?>
